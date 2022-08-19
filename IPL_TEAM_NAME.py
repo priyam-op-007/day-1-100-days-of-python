79770369586d70226e376cbc9f71eae973ed26b5
Priyaranjan Mishra <priyaranjanmishra000@gmail.com>
2022-08-19 17:35:02 +0530

    Create IPL_TEAM_NAME.py

---
+++ IPL_TEAM_NAME.py
@@ -0,0 +1,17 @@
+Input -
+
+print("Welcome to IPL team name generator")
+city = input('What is the city that you grew up in?\n')
+pet = input('What is your favourite one thing in the world?\n')
+IPL_team_name = city + " " + pet
+print("your IPL team name is - "+IPL_team_name)
+
+
+Output - 
+
+Welcome to IPL team name generator
+What is the city that you grew up in?
+Ankleshwar
+What is your favourite one thing in the world?
+Warrior
+your IPL team name is - Ankleshwar Warrior
